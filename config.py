dcf8b7ead2c778bd951a6ad470d85ca373b0a757
Robialta <obyaltha@gmail.com>
2018-07-16 10:15:59 +0100

    first commit

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config(object):
-    SECRET_KEY = os.environ.get['SECRET_KEY'] or 'aplikasipmb'
-    SQLALCHEMY_DATABASE_URI = os.environ.get['DATABASE_URL']  or 'mysql+pymysql://root:@localhost/pmb'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'aplikasipmb'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root:@localhost/pmb'
     SQLALCHEMY_TRACK_MODIFICATION = False
